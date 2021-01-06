3f90313f76d77753c2e9f89b689f08a1f334d8d7
DiyaSejpal <76207044+DiyaSejpal@users.noreply.github.com>
2021-01-06 20:27:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+var bg , bgIMG;
+var cat,mouse;
+var catIMG1 , catIMG2 , catIMG3 ;
+var mouseIMG1 , mouseIMG2 , mouseIMG3;
+
+
+function preload() {
+    //load the images here
+   bgIMG = loadImage("images/garden.png");
+
+   catIMG1 = loadAnimation("images/tomOne.png");
+   catIMG2 = loadAnimation("images/tomTwo.png","images/tomThree.png");
+   catIMG3 = loadAnimation("images/tomFour.png");
+   mouseIMG1 = loadAnimation("images/jerryOne.png");
+   mouseIMG2 = loadAnimation("images/jerryTwo.png","images/jerryThree.png");
+   mouseIMG3 = loadAnimation("images/jerryFour.png")
+
+
+
+}
+
+function setup(){
+    createCanvas(1000,800);
+    
+    bg = createSprite(500,400,1000,800);
+    bg.addImage("bg",bgIMG);
+    
+    cat = createSprite(870,600);
+    cat.addAnimation("tomSleeping",catIMG1);
+    cat.scale = 0.2;
+
+    mouse = createSprite(200,600);
+    mouse.addAnimation("jerryStanding",mouseIMG1);
+    mouse.scale = 0.15;
+
+    
+
+}
+
+function draw() {
+
+    if(cat.x - mouse.x < (cat.width - mouse.width)/2){
+
+        cat.velocityX = 0;
+        cat.addAnimation("tomLastImage",catIMG3);
+        cat.x = 300;
+        cat.scale = 0.2;
+        cat.changeAnimation("tomLastImage");
+        mouse.addAnimation("jerryLastImage",mouseIMG3);
+        mouse.scale = 0.15;
+        mouse.changeAnimation("jerryLastImage");
+    }
+    
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+    if(keyCode === LEFT_ARROW){
+        cat.velocityX = -5;
+        cat.addAnimation("tomRunning",catIMG2);
+        cat.changeAnimation("tomRunning");
+
+        mouse.addAnimation("jerryTeasing",mouseIMG2);
+        mouse.changeAnimation("jerryTeasing");
+    }
+
+  
+
+}
